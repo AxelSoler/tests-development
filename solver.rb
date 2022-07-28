9d6edc90aeb84be0509443fdbbe1f7449dfff335
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-07-28 12:21:24 -0300

    second test

--- solver.rb
+++ solver.rb
@@ -1,11 +1,13 @@
 class Solver
   def factorial(val)
-    return unless val > 1
-
-    answer = 1
-    (2..val).each do |i|
-      answer *= i
+    if val > 1
+      answer = 1
+      (2..val).each do |i|
+        answer *= i
+      end
+      answer
+    elsif [0, 1].include? val
+      1
     end
-    answer
   end
 end
