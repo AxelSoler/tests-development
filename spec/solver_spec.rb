517ef125ee0dc3e6e1bd24e76f550eba9531d3f5
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-07-28 12:03:21 -0300

    tests for "factorial" method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,4 +8,22 @@ describe Solver do
       expect(solver).to be_truthy
     end
   end
+  context 'When testing the factorial method' do
+    it 'should return the factorial of 3' do
+      answer = solver.factorial(3)
+      expect(answer).to eq 6
+    end
+    it 'should return the factorial of 0' do
+      answer = solver.factorial(0)
+      expect(answer).to eq 1
+    end
+    it 'the factorial of -4 should return an exception' do
+      answer = solver.factorial(-4)
+      expect { answer }.to raise_error(StandardError)
+    end
+    it 'the factorial of 3.14 should return an exception' do
+      answer = solver.factorial(3.14)
+      expect { answer }.to raise_error(StandardError)
+    end
+  end
 end
