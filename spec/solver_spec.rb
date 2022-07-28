155cdfa9e2f25e0905969acf1c6dc41126115988
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-28 10:52:27 -0500

    Add reverse method tests

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,11 +3,13 @@ require_relative '../solver'
 
 describe Solver do
   solver = Solver.new
+
   context 'When testing the solver class' do
     it 'expect the class solver to exist' do
       expect(solver).to be_truthy
     end
   end
+
   context 'When testing the factorial method' do
     it 'should return the factorial of 3' do
       answer = solver.factorial(3)
@@ -24,12 +26,16 @@ describe Solver do
       expect { solver.factorial(3.14) }.to raise_error('Integer values only')
     end
   end
-  # context 'When testing the reverse method' do
-  #   it 'should contain an @status instance variable equal to "Creatd"' do
-  #     app = App.new
-  #     expect(app.status).to eq 'Created'
-  #   end
-  # end
+
+  context 'When testing the reverse method' do
+    it 'return a string in reverse order' do
+      expect(solver.reverse('word')).to eq 'drow'
+    end
+    it 'raise an exception if the input is not a string' do
+      expect { solver.reverse(3) }.to raise_error('String values only')
+    end
+  end
+
   # context 'When testing the fizzbuzz method' do
   #   it 'should contain an @status instance variable equal to "Creatd"' do
   #     app = App.new
